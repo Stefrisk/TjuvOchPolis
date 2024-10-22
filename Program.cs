2051c753dd089f2f8f4e52973c8ea8d7daaed129
stefan frisk <stefrik@gmail.com>
2024-10-22 21:10:16 +0200

    no changes

--- Program.cs
+++ Program.cs
@@ -5,11 +5,11 @@
         static void Main(string[] args)
         {
             Town town = new Town();
+            
             string item = Console.ReadLine();
             Console.WriteLine("Cops and Robbers");
             Town.PrintTown(town);
-            Console.ReadLine();
-            
+                      
             Citizen citizen = new Citizen(3,3);
             for (int i = 0; i < citizen.Inventory.Count; i++) 
             {
