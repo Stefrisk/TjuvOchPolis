4729c02ab62448f01d14a8ce2054f13f7f498ca5
Stefrisk <stefrisk@gmail.com>
2024-10-22 11:49:08 +0200

    no changes made

---
+++ Item.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Item
+{
+    public string Namn { get; set; }
+
+    public Item(string namn)
+    {
+        Namn = namn;
+    }
+}
+
+public class Klocka: Item
+{
+    public Klocka(string namn) : base(namn)
+    {
+        
+    }
+}
+
+public class Pengar : Item
+{
+    public Pengar(string namn) : base(namn)
+    {
+        
+    }
+}
+
+public class Mobil : Item
+{
+    public Mobil(string namn) : base(namn)
+    {
+        
+    }
+}
+public class Nycklar : Item
+{
+    public Nycklar(string namn) : base(namn)
+    {
+        
+    }
+}
+
