7f86106e60e7befd68823e45c8ee18c0b49aa17e
Stefrisk <stefrisk@gmail.com>
2024-10-23 15:52:26 +0200

    added method that creates several people of 3 types, added a list of people that will be in "town"

--- Program.cs
+++ Program.cs
@@ -2,22 +2,44 @@
 {
     internal class Program
     {
+        public static void MakePeople(int AmountOfRobbers, int AmountOfCitizens, int AmountOfPolis, ref List<Person> PeopleInTown)
+        {
+            for (int i = 0; i < AmountOfRobbers; i++)
+            {
+                Robber robber = new Robber();
+                PeopleInTown.Add(robber);
+            }
+            for (int i = 0;i < AmountOfCitizens; i++)
+            {
+                Citizen citizen = new Citizen();
+                PeopleInTown.Add(citizen);
+            }
+            for(int i = 0; i < AmountOfPolis; i++)
+            {
+                Police police = new Police();
+                PeopleInTown.Add(police);
+            }
+        }
         static void Main(string[] args)
         {
+            int AmountofRobbers = 10;
+            int AmountofCitizens = 20;
+            int AmountofPolis = 15;
+            List<Person> PeopleInTown = new List<Person>();
             Town town = new Town();
+            MakePeople(AmountofRobbers, AmountofCitizens, AmountofPolis, ref PeopleInTown);
             
-            string item = Console.ReadLine();
+
+            
             Console.WriteLine("Cops and Robbers");
             Town.PrintTown(town);
-                      
-            Citizen citizen = new Citizen(3,3);
-            town.Persons.Add(citizen);
-
-            for (int i = 0; i < citizen.Inventory.Count; i++) 
+            foreach (Person person in PeopleInTown) 
             {
-                Console.WriteLine(citizen.Inventory[i].Namn );
+                Console.WriteLine(person.Name);           
             }
-            Console.WriteLine(citizen.Name);
+                      
+            
+           
             Console.ReadLine();
    
         }
