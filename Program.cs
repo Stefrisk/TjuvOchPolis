47547a3efd7656a4e773a2c3057f876a65ad3545
Stefrisk <stefrisk@gmail.com>
2024-10-28 18:11:59 +0100

    added while loop, the toen and players print and move succesfuly

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace TjuvOchPolis
+﻿using System.Runtime.InteropServices;
+
+namespace TjuvOchPolis
 {
     internal class Program
     {
@@ -9,39 +11,49 @@
                 Robber robber = new Robber();
                 PeopleInTown.Add(robber);
             }
-            for (int i = 0;i < AmountOfCitizens; i++)
+            for (int i = 0; i < AmountOfCitizens; i++)
             {
                 Citizen citizen = new Citizen();
                 PeopleInTown.Add(citizen);
             }
-            for(int i = 0; i < AmountOfPolis; i++)
+            for (int i = 0; i < AmountOfPolis; i++)
             {
                 Police police = new Police();
                 PeopleInTown.Add(police);
             }
         }
+       
         static void Main(string[] args)
         {
             int AmountofRobbers = 10;
             int AmountofCitizens = 20;
             int AmountofPolis = 15;
-            List<Person> PeopleInTown = new List<Person>();
-            Town town = new Town();
-            MakePeople(AmountofRobbers, AmountofCitizens, AmountofPolis, ref PeopleInTown);
+            List<Person> PeopleInTown = new List<Person>();                     //make list of people
+            Town town = new Town();                                             //Make town
+            MakePeople(AmountofRobbers, AmountofCitizens, AmountofPolis, ref PeopleInTown);//make people and add to list
+            town.PlayerLocations = Town.PlayerLocation(PeopleInTown,town.PlayerLocations); // save player start locations to town 
             
-
             
-            Console.WriteLine("Cops and Robbers");
-            Town.PrintTown(town);
-            foreach (Person person in PeopleInTown) 
+            while (true)
             {
-                Console.WriteLine(person.Name);           
+                Console.Clear();
+                
+                Console.WriteLine("Cops and Robbers");
+
+                Person.Move(PeopleInTown); // player moves onestep 
+                Town.PlayerLocation2(PeopleInTown, ref town._playerLocations);// saves updated player location to the array
+                Town.PrintTown(town);    //Print Town and player locations 
+                Thread.Sleep(200);
+
             }
-                      
-            
-           
-            Console.ReadLine();
-   
+
+
+
+
+
+
+
+
         }
     }
 }
