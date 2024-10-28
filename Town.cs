17db15a17bd0bbdd053b2c208c8bebf1b1f46493
Stefrisk <stefrisk@gmail.com>
2024-10-28 18:13:34 +0100

    get and set changed in properties, method to update player location array made, print town small changes

--- Town.cs
+++ Town.cs
@@ -19,7 +19,7 @@ namespace TjuvOchPolis
             }
             set
             {
-                
+                _karta = value;
             }
         }
         public string[,] _playerLocations = new string[27, 102];
@@ -31,7 +31,7 @@ namespace TjuvOchPolis
             }
             set
             {
-                
+                _playerLocations = value;
             }
         }
         public static void PrintTown(Town town)
@@ -50,31 +50,90 @@ namespace TjuvOchPolis
                     {
                         town.Karta[i, j] = "|";
                     }
-                    else if (town.Karta[i, j] == null && town.PlayerLocations[i, j] == "c")
+                    else if (!string.IsNullOrEmpty(town.PlayerLocations[i, j]))
                     {
-                        town.Karta[i, j] = "C";
+                        town.Karta[i, j] = town.PlayerLocations[i,j];
                     }
                     else
                     {
                         town.Karta[i, j] = " ";
                     }
+                  
+                    
 
 
                 }
 
 
             }
+           
             for (int i = 0; i < town.Karta.GetLength(0); i++)
             {
                 for (int j = 0; j < town.Karta.GetLength(1); j++)
                 {
                     Console.Write(town.Karta[i, j]);
+                    
                 }
                 Console.WriteLine();
             }
 
 
 
+        }
+        public static void PlayerLocation2(List<Person> ListOfPeople, ref string[,] PlayerLocations)
+        {
+            Array.Clear(PlayerLocations,0,PlayerLocations.Length);
+            for (int i = 0; i < ListOfPeople.Count; i++) 
+            {
+                
+                
+
+                int x = ListOfPeople[i].XLocation;
+               int y = ListOfPeople[i].YLocation;
+               
+                if (ListOfPeople[i] is Robber)
+                {
+                    PlayerLocations[x, y] = "R";
+                }
+                if (ListOfPeople[i] is Citizen)
+                {
+                    PlayerLocations[x, y] = "C";
+                }
+                if (ListOfPeople[i] is Police)
+                {
+                    PlayerLocations[x, y] = "P";
+                }
+                
+            }
+        }
+        public static string[,] PlayerLocation(List<Person> ListOfPeople,string[,] PlayerLocations)
+        {
+            
+            for (int i = 0; i < ListOfPeople.Count; i++)
+            {
+               
+               
+
+                
+                int x = ListOfPeople[i].XLocation;
+                int y = ListOfPeople[i].YLocation;
+                
+
+                if (ListOfPeople[i] is Robber)
+                {
+                    PlayerLocations[x, y] = "R";
+                }
+                if (ListOfPeople[i] is Citizen)
+                {
+                    PlayerLocations[x, y] = "C";
+                }
+                if (ListOfPeople[i] is Police)
+                {
+                    PlayerLocations[x, y] = "P";
+                }
+
+            }
+            return PlayerLocations;
         }
     }
 }
