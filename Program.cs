0b73daa515bb3231a0a0eaabbdae2f5a1d357bf2
Yousif Hirmiz <hirmizyousif123@gmail.com>
2024-10-29 17:45:03 +0100

    La till en metod för att hantera interactions mellan tjuv och citizen

--- Program.cs
+++ Program.cs
@@ -22,38 +22,51 @@ namespace TjuvOchPolis
                 PeopleInTown.Add(police);
             }
         }
-       
+
+        private static void RobberCitizenInteractions(List<Person> peopleInTown, Town town)
+        {
+            for (int i = 0; i < peopleInTown.Count; i++)
+            {             
+                if (peopleInTown[i] is Robber robber)
+                {
+                    int x = robber.XLocation;
+                    int y = robber.YLocation;
+
+                    if (town.PlayerLocations[x, y] is Citizen citizen)
+                    {
+                        robber.Inventory = Town.RobItem(robber, citizen);
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             int AmountofRobbers = 10;
             int AmountofCitizens = 20;
             int AmountofPolis = 15;
             List<Person> PeopleInTown = new List<Person>();                     //make list of people
-            Town town = new Town();                                             //Make town
+            Town town = new Town();                                             //Make town      
             MakePeople(AmountofRobbers, AmountofCitizens, AmountofPolis, ref PeopleInTown);//make people and add to list
             town.PlayerLocations = Town.PlayerLocation(PeopleInTown,town.PlayerLocations); // save player start locations to town 
             
-            
+
             while (true)
             {
                 Console.Clear();
                 
-                Console.WriteLine("Cops and Robbers");
+                Console.WriteLine("Cops and Robbers");              
+                Console.WriteLine();
 
-                Person.Move(PeopleInTown); // player moves onestep 
-                Town.PlayerLocation2(PeopleInTown, ref town._playerLocations);// saves updated player location to the array
-                Town.PrintTown(town);    //Print Town and player locations 
-                Thread.Sleep(200);
+                Person.Move(PeopleInTown);                                          // player moves onestep 
+                
+                Town.PlayerLocation(PeopleInTown, town._playerLocations);           // saves updated player location to the array                
 
+                RobberCitizenInteractions(PeopleInTown, town);
+
+                Town.PrintTown(town);                                               //Print Town and player locations 
+                Thread.Sleep(100);
             }
-
-
-
-
-
-
-
-
         }
     }
 }
