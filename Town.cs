7fbae9858dd78708bcebd1ea63c197f32ae69eb5
Yousif Hirmiz <hirmizyousif123@gmail.com>
2024-10-29 17:47:41 +0100

    Ändrade borders, ändrade så att PlayerLocations matrisen är nu av datatypen "Person" och inte string, la till en metod som hanterar överföringen av citizens item till tjuvens inventory, la till en initiatePlayerLocation metod för att initiera alla cells i PlayerLocations matrisen

--- Town.cs
+++ Town.cs
@@ -22,8 +22,8 @@ namespace TjuvOchPolis
                 _karta = value;
             }
         }
-        public string[,] _playerLocations = new string[27, 102];
-        public string[,] PlayerLocations
+        public Person[,] _playerLocations = new Person[27, 102];
+        public Person[,] PlayerLocations
         {
             get
             {
@@ -34,37 +34,47 @@ namespace TjuvOchPolis
                 _playerLocations = value;
             }
         }
+
+        public Town()
+        {
+            InitiatePlayerLocation(ref _playerLocations);
+        }
+
         public static void PrintTown(Town town)
         {
-            
+            for (int i = 0; i < town.Karta.GetLength(0); i++)
+            {
+                for (int j = 0; j < town.Karta.GetLength(1); j++)
+                {
+                    town.Karta[i, j] = " "; // Fyll med tomma strängar
+                }
+            }
 
             for (int i = 0; i < town.Karta.GetLength(0); i++)
             {
                 for (int j = 0; j < town.Karta.GetLength(1); j++)
                 {
-                    if (i == 0 || i == 26)
+                    if (i == 0)
                     {
-                        town.Karta[i, j] = "-";
+                        town.Karta[i, j] = "_";
+                    }
+                    else if (i == 26)
+                    {
+                        town.Karta[i, j] = "¯";
                     }
                     else if (j == 0 || j == 101)
                     {
                         town.Karta[i, j] = "|";
-                    }
-                    else if (!string.IsNullOrEmpty(town.PlayerLocations[i, j]))
+                    }                   
+                    else if (town.PlayerLocations != null && town.PlayerLocations[i, j] != null && !string.IsNullOrEmpty(town.PlayerLocations[i, j].Character))
                     {
-                        town.Karta[i, j] = town.PlayerLocations[i,j];
+                        town.Karta[i, j] = town.PlayerLocations[i, j].Character;
                     }
-                    else
+                    else 
                     {
                         town.Karta[i, j] = " ";
-                    }
-                  
-                    
-
-
+                    }                            
                 }
-
-
             }
            
             for (int i = 0; i < town.Karta.GetLength(0); i++)
@@ -76,66 +86,62 @@ namespace TjuvOchPolis
                 }
                 Console.WriteLine();
             }
-
-
-
         }
-        public static void PlayerLocation2(List<Person> ListOfPeople, ref string[,] PlayerLocations)
+        
+        public static Person[,] PlayerLocation(List<Person> ListOfPeople, Person[,] PlayerLocations)
         {
-            Array.Clear(PlayerLocations,0,PlayerLocations.Length);
-            for (int i = 0; i < ListOfPeople.Count; i++) 
-            {
-                
-                
-
-                int x = ListOfPeople[i].XLocation;
-               int y = ListOfPeople[i].YLocation;
-               
-                if (ListOfPeople[i] is Robber)
-                {
-                    PlayerLocations[x, y] = "R";
-                }
-                if (ListOfPeople[i] is Citizen)
-                {
-                    PlayerLocations[x, y] = "C";
-                }
-                if (ListOfPeople[i] is Police)
-                {
-                    PlayerLocations[x, y] = "P";
-                }
-                
-            }
-        }
-        public static string[,] PlayerLocation(List<Person> ListOfPeople,string[,] PlayerLocations)
-        {
-            
+            Array.Clear(PlayerLocations, 0, PlayerLocations.Length);
             for (int i = 0; i < ListOfPeople.Count; i++)
             {
-               
-               
 
-                
                 int x = ListOfPeople[i].XLocation;
                 int y = ListOfPeople[i].YLocation;
-                
 
+                PlayerLocations[x, y] = ListOfPeople[i];
+
+               
                 if (ListOfPeople[i] is Robber)
                 {
-                    PlayerLocations[x, y] = "R";
+                    
+                     if (PlayerLocations[x, y] is Citizen citizen)
+                     {
+                         ListOfPeople[i].Inventory = RobItem((Robber)ListOfPeople[i], citizen);
+                     }
                 }
-                if (ListOfPeople[i] is Citizen)
-                {
-                    PlayerLocations[x, y] = "C";
-                }
-                if (ListOfPeople[i] is Police)
-                {
-                    PlayerLocations[x, y] = "P";
-                }
-
+             
             }
             return PlayerLocations;
         }
+
+
+        public static List<Item> RobItem (Person robber, Citizen citizen)
+        {
+            Random rnd = new Random();
+            if(citizen.Inventory.Any())
+            {
+                Item item = citizen.Inventory[rnd.Next(0, citizen.Inventory.Count)];
+                citizen.Inventory.Remove(item);
+                robber.Inventory.Add(item);
+
+                Console.WriteLine($"Tjuven {robber.Name} stal {item.Namn} från {citizen.Name}");
+                Thread.Sleep(3500);
+            }
+            return robber.Inventory;
+        }
+
+        public static void InitiatePlayerLocation(ref Person[,] PlayerLocations)
+        {
+            for (int i = 0; i < PlayerLocations.GetLength(0); i++)
+            {
+                for (int j = 0; j < PlayerLocations.GetLength(1); j++)
+                {
+                    PlayerLocations[i, j] = new Person(); // Lägg till en tom `Person`-instans
+                }
+            }
+
+        }
     }
+
 }
 
 
