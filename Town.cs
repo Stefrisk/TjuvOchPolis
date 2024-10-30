f9b4571ff060b4345300cf693cddae4901a844f4
Yousif Hirmiz <hirmizyousif123@gmail.com>
2024-10-30 14:29:05 +0100

    Tog bort konstruktor som anropar gamla metoden InitiatiePlayerLocations(behövs inte) och la till en metod som hanterar överföringen av robbers inventory till police inventory (ConfiscateItem)

--- Town.cs
+++ Town.cs
@@ -9,7 +9,7 @@ namespace TjuvOchPolis
 {
     public class Town
     {
-        
+
         public string[,] _karta = new string[27, 102];
         public string[,] Karta
         {
@@ -35,10 +35,7 @@ namespace TjuvOchPolis
             }
         }
 
-        public Town()
-        {
-            InitiatePlayerLocation(ref _playerLocations);
-        }
+       
 
         public static void PrintTown(Town town)
         {
@@ -65,85 +62,81 @@ namespace TjuvOchPolis
                     else if (j == 0 || j == 101)
                     {
                         town.Karta[i, j] = "|";
-                    }                   
+                    }
                     else if (town.PlayerLocations != null && town.PlayerLocations[i, j] != null && !string.IsNullOrEmpty(town.PlayerLocations[i, j].Character))
                     {
                         town.Karta[i, j] = town.PlayerLocations[i, j].Character;
                     }
-                    else 
+                    else
                     {
                         town.Karta[i, j] = " ";
-                    }                            
+                    }
                 }
             }
-           
+
             for (int i = 0; i < town.Karta.GetLength(0); i++)
             {
                 for (int j = 0; j < town.Karta.GetLength(1); j++)
                 {
                     Console.Write(town.Karta[i, j]);
-                    
+
                 }
                 Console.WriteLine();
             }
         }
-        
+
         public static Person[,] PlayerLocation(List<Person> ListOfPeople, Person[,] PlayerLocations)
         {
             Array.Clear(PlayerLocations, 0, PlayerLocations.Length);
             for (int i = 0; i < ListOfPeople.Count; i++)
             {
-
                 int x = ListOfPeople[i].XLocation;
                 int y = ListOfPeople[i].YLocation;
 
                 PlayerLocations[x, y] = ListOfPeople[i];
 
-               
-                if (ListOfPeople[i] is Robber)
-                {
-                    
-                     if (PlayerLocations[x, y] is Citizen citizen)
-                     {
-                         ListOfPeople[i].Inventory = RobItem((Robber)ListOfPeople[i], citizen);
-                     }
-                }
-             
             }
+
             return PlayerLocations;
         }
 
 
-        public static List<Item> RobItem (Person robber, Citizen citizen)
+        public static List<Item> RobItem(Person robber, Citizen citizen)
         {
             Random rnd = new Random();
-            if(citizen.Inventory.Any())
+            if (citizen.Inventory.Any())
             {
                 Item item = citizen.Inventory[rnd.Next(0, citizen.Inventory.Count)];
                 citizen.Inventory.Remove(item);
                 robber.Inventory.Add(item);
 
                 Console.WriteLine($"Tjuven {robber.Name} stal {item.Namn} från {citizen.Name}");
-                Thread.Sleep(3500);
+                Thread.Sleep(350);
             }
             return robber.Inventory;
         }
 
-        public static void InitiatePlayerLocation(ref Person[,] PlayerLocations)
+        public static List<Item> ConfiscateItem(Person police, Robber robber)
         {
-            for (int i = 0; i < PlayerLocations.GetLength(0); i++)
+            Console.WriteLine("Här kom jag in!");
+            Thread.Sleep(1000);
+            if (robber.Inventory.Any())
             {
-                for (int j = 0; j < PlayerLocations.GetLength(1); j++)
+                for (int i = 0; i < robber.Inventory.Count; i++)
                 {
-                    PlayerLocations[i, j] = new Person(); // Lägg till en tom `Person`-instans
+                    Item item = robber.Inventory[i];
+                    robber.Inventory.Remove(item);
+                    police.Inventory.Add(item);
+                    Console.WriteLine($"Polisen {police.Name} beslagtog {item.Namn} från {robber.Name}");
+                    Thread.Sleep(1000);
                 }
-            }
 
+                Thread.Sleep(2500);
+            }
+            return police.Inventory;
         }
     }
-
 }
-
 
 
 
