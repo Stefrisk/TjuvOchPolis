bb701327ae7e9c3831c4f7066556556136122ae2
Stefrisk <stefrisk@gmail.com>
2024-11-01 15:46:52 +0100

    added int props for player counters

--- Town.cs
+++ Town.cs
@@ -9,8 +9,25 @@ namespace TjuvOchPolis
 {
     public class Town
     {
+        public int PlayersOnMap 
+        {
+            get;
+            set; 
+        }
+        public int RobbersInJail { get; set; } = 0;
+        public int RobbersTaken { get; set; } = 0;
+        public int RobbersOnMap { get; set; } = 0;
+        public int CitzensOnMap { get; set; } = 0;
+        public int PoliceOnMap { get; set; } = 0;
+        public int CitizensRobbed { get; set; } = 0;
+        public string[,] _jail = new string[11, 11];
+        public string[,] Jail
+        {
+            get {  return _jail; }
+            set {  _jail = value; } 
+        }
 
-        public string[,] _karta = new string[27, 122];
+        public string[,] _karta = new string[27, 102];
         public string[,] Karta
         {
             get
@@ -22,7 +39,7 @@ namespace TjuvOchPolis
                 _karta = value;
             }
         }
-        public Person[,] _playerLocations = new Person[27, 122];
+        public Person[,] _playerLocations = new Person[27, 102];
         public Person[,] PlayerLocations
         {
             get
@@ -60,22 +77,11 @@ namespace TjuvOchPolis
                     {
                         town.Karta[i, j] = "¯";
                     }
-                    else if (j == 0 || j == 101 || j == 121)
+                    else if (j == 0 || j == 101 )
                     {
                         town.Karta[i, j] = "|";
                     }
-                    else if(j == 105 && i < 11)
-                    {
-                        town.Karta[i, j] = "|";
-                    }
-                    else if(i == 11 && j > 105)
-                    {
-                        town.Karta[i, j] = "¯";
-                    }
-                    else if (i == 0 && j > 105)
-                    {
-                        town.Karta[i, j] = "_";
-                    }                  
+                                     
                     else if (town.PlayerLocations != null && town.PlayerLocations[i, j] != null && !string.IsNullOrEmpty(town.PlayerLocations[i, j].Character))
                     {
                         town.Karta[i, j] = town.PlayerLocations[i, j].Character;
@@ -87,18 +93,26 @@ namespace TjuvOchPolis
                 }
             }
             
-            town.Karta[13, 109] = "P";
-            town.Karta[13, 110] = "R";
-            town.Karta[13, 111] = "I";
-            town.Karta[13, 112] = "S";
-            town.Karta[13, 113] = "O";
-            town.Karta[13, 114] = "N";
+            //town.Karta[13, 109] = "P";
+            //town.Karta[13, 110] = "R";
+            //town.Karta[13, 111] = "I";
+            //town.Karta[13, 112] = "S";
+            //town.Karta[13, 113] = "O";
+            //town.Karta[13, 114] = "N";
+            
+            
+            
+           Console.WriteLine($"Players on map:{town.PlayersOnMap}  Citizens:{town.CitzensOnMap}  Police:{town.PoliceOnMap}  Robbers:{town.RobbersOnMap} \n Citizens robbed:{town.CitizensRobbed}  Robbers taken:{town.RobbersTaken}  Robbers in jail:{town.RobbersInJail}");
+            
 
             for (int i = 0; i < town.Karta.GetLength(0); i++)
             {
                 for (int j = 0; j < town.Karta.GetLength(1); j++)
                 {
+                    
                     Console.Write(town.Karta[i, j]);
+                    
+
 
                 }
                 Console.WriteLine();
@@ -107,13 +121,55 @@ namespace TjuvOchPolis
             int itemsToPrint = Math.Min(5, recentInteractions.Length);
             for (int i = 0; i < itemsToPrint; i++)
             {
-                Console.WriteLine(recentInteractions[i]);
+                Console.WriteLine(recentInteractions[i]);  // prints interactions 
+            }
+            for (int i = 0;i < town.Jail.GetLength(0); i++) // fill jail string array with map edges 
+            {
+                for(int j = 0;j < town.Jail.GetLength(1); j++)
+                {    if (i == 0)
+                    {
+                        town.Jail[i, j] = "_";
+                    }
+                    else if(i == 10)
+                    {
+                        town.Jail[i, j] = "¯";
+                    }
+                   else if (j == 0 || j == 10)
+                    {
+                        town.Jail[i, j] = "|";
+
+                    }
+                    //else if ( != null && town.PlayerLocations[i, j] != null && !string.IsNullOrEmpty(town.PlayerLocations[i, j].Character))
+                    //{
+                    //    town.Karta[i, j] = town.PlayerLocations[i, j].Character;                          print player jail location!!!
+                    //}
+                    else 
+                    {
+                        town.Jail[i, j] = " "; 
+                            
+                            
+                    }
+                   
+                    
+                }
+            }
+            for (int i = 0; i < town.Jail.GetLength(0); i++)
+            {
+                for (int j = 0; j < town.Jail.GetLength(1); j++)
+                    
+                {
+                    Console.Write(town.Jail[i, j]);
+
+                }
+                Console.WriteLine();
             }
             
         }
 
-        public static Person[,] PlayerLocation(List<Person> ListOfPeople, Person[,] PlayerLocations)
+        public static Person[,] PlayerLocation(List<Person> ListOfPeople, Person[,] PlayerLocations, Town town)
         {
+            town.PlayersOnMap = ListOfPeople.Count;
+            
             Array.Clear(PlayerLocations, 0, PlayerLocations.Length);
             for (int i = 0; i < ListOfPeople.Count; i++)
             {
